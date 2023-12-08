524988a19a185d16933479d48163d9d406d2f8ef
Thomas Turner <thomastdt@gmail.com>
2023-12-08 02:41:03 -0800

    linode_vpn.tf: Test passed to destroy instance
    Needed to get variables / info on what instance(s) will be destroyed
    so that a terraform resource "null_resource" "cleanup_script"
    can automatically update the ssh config file by removing
    the hostname login information
    
    Some helpful resources:
    
    https://registry.terraform.io/providers/hashicorp/null/latest/docs/resources/resource#example-usage
    https://www.justinmklam.com/posts/2022/05/terraform-null-resource/
    https://developer.hashicorp.com/terraform/language/resources/provisioners/syntax
    https://stackoverflow.com/questions/64436987/invalid-reference-from-destroy-provisioner
    https://stackoverflow.com/questions/64952745/terraform-and-references-in-local-exec-destroy-provisioners
    
    Signed-off-by: Thomas Turner <thomastdt@gmail.com>

--- linode_vpn.tf
+++ linode_vpn.tf
@@ -59,86 +59,117 @@ resource "linode_instance" "vpn_instance" {
     root_pass       = var.vpn_instances_keys[count.index]["root_pass"]
     swap_size       = var.vpn_instances[count.index]["swap_size"]
 
-    connection {
-      type        = "ssh"
-      user        = "root"
-      private_key = "${file(var.vpn_instances_keys[count.index]["ssh_private_key"])}"
-      host        = self.ip_address 
-    }
-
-    provisioner "file" {
-      source      = "${var.vpn_instances[count.index]["env_file_path"]}"
-      destination = "/root/.env"
-    }
-    
-    provisioner "file" {
-      source      = "${var.vpn_instances_keys[count.index]["ssh_private_key"]}.pub"
-      destination = "/root/key.pub"
-    }
-
-    provisioner "file" {
-      source      = "setup.sh"
-      destination = "/root/setup.sh"
-    }
-
-    provisioner "file" {
-      source      = "openvpn-install.sh"
-      destination = "/root/openvpn-install.sh"
-    }
-
-    provisioner "remote-exec" {
-      inline = [
-        "cat /etc/os-release",
-        "chmod +x /root/setup.sh",
-        "chmod +x /root/openvpn-install.sh"
-      ]
-    }
-
-     provisioner "local-exec" {
-      command = <<EOT
-      
-        ansible-playbook -u root -i '${self.ip_address},' \
-        -e 'ansible_python_interpreter=/usr/bin/python3' \
-        -e 'client_name=${var.vpn_instances[count.index]["vpn_client_name"]}' \
-        --private-key '${var.vpn_instances_keys[count.index]["ssh_private_key"]}' \
-        upgrade.yml 
-
-      EOT
-    }
-
     provisioner "local-exec" {
+      when = destroy
       command = <<EOT
-        echo ""
-        echo "creating/updating the ssh config file to include our new vpn server"
-        echo "exporting env variables that sshConfig.sh will use"
-        . "${var.vpn_instances[count.index]["env_file_path"]}"
-        export VPN_LABEL=${var.vpn_instances[count.index]["label"]}
-        export CLIENT=${var.vpn_instances[count.index]["vpn_client_name"]}
-        export VPN_IP=${self.ip_address}
-        export PRIV_KEY_PATH=${var.vpn_instances_keys[count.index]["ssh_private_key"]}
-        
-        chmod +x ./sshConfig.sh
-        bash ./sshConfig.sh
-        echo ""
-        echo "you can now ssh into your vpn server using..."
-        echo "ssh ${var.vpn_instances[count.index]["label"]}"
-        echo "you can edit this file at: ~/.ssh/config"
-
-        echo ""
-        echo "building ./cleanup.sh script"
-        echo "" >> ./cleanup.sh
-        echo "ssh-keygen -f \"/home/$USER/.ssh/known_hosts\" -R \"${self.ip_address}\"" >> ./cleanup.sh
-        chmod +x ./cleanup.sh
-        echo ""
-        echo "You can delete the server instance by executing:"
-        echo "./cleanup.sh"
-        echo ""
-        echo "Note:"
-        echo "You will need to manually delete your private and public keys in ~/.ssh/ folder"
-        echo "...or reuse them."
-       
+        echo 'Removing Host: ${self.ip_address} from ssh config file'
       EOT
     }
+  }
+
+    # connection {
+    #   type        = "ssh"
+    #   user        = "root"
+    #   private_key = "${file(var.vpn_instances_keys[count.index]["ssh_private_key"])}"
+    #   host        = self.ip_address
+    # }
+
+    # provisioner "file" {
+    #   source      = "${var.vpn_instances[count.index]["env_file_path"]}"
+    #   destination = "/root/.env"
+    # }
+    
+    # provisioner "file" {
+    #   source      = "${var.vpn_instances_keys[count.index]["ssh_private_key"]}.pub"
+    #   destination = "/root/key.pub"
+    # }
+
+    # provisioner "file" {
+    #   source      = "setup.sh"
+    #   destination = "/root/setup.sh"
+    # }
+
+    # provisioner "file" {
+    #   source      = "openvpn-install.sh"
+    #   destination = "/root/openvpn-install.sh"
+    # }
+
+    # provisioner "remote-exec" {
+    #   inline = [
+    #     "cat /etc/os-release",
+    #     "chmod +x /root/setup.sh",
+    #     "chmod +x /root/openvpn-install.sh"
+    #   ]
+    # }
+
+    #  provisioner "local-exec" {
+    #   command = <<EOT
+      
+    #     ansible-playbook -u root -i '${self.ip_address},' \
+    #     -e 'ansible_python_interpreter=/usr/bin/python3' \
+    #     -e 'client_name=${var.vpn_instances[count.index]["vpn_client_name"]}' \
+    #     --private-key '${var.vpn_instances_keys[count.index]["ssh_private_key"]}' \
+    #     upgrade.yml
+
+    #   EOT
+    # }
+
+    # provisioner "local-exec" {
+    #   command = <<EOT
+    #     echo ""
+    #     echo "creating/updating the ssh config file to include our new vpn server"
+    #     echo "exporting env variables that sshConfig.sh will use"
+    #     . "${var.vpn_instances[count.index]["env_file_path"]}"
+    #     export VPN_LABEL=${var.vpn_instances[count.index]["label"]}
+    #     export CLIENT=${var.vpn_instances[count.index]["vpn_client_name"]}
+    #     export VPN_IP=${self.ip_address}
+    #     export PRIV_KEY_PATH=${var.vpn_instances_keys[count.index]["ssh_private_key"]}
+        
+    #     chmod +x ./sshConfig.sh
+    #     bash ./sshConfig.sh
+    #     echo ""
+    #     echo "you can now ssh into your vpn server using..."
+    #     echo "ssh ${var.vpn_instances[count.index]["label"]}"
+    #     echo "you can edit this file at: ~/.ssh/config"
+
+    #     echo ""
+    #     echo "building ./cleanup.sh script"
+    #     echo "" >> ./cleanup.sh
+    #     echo "ssh-keygen -f \"/home/$USER/.ssh/known_hosts\" -R \"${self.ip_address}\"" >> ./cleanup.sh
+    #     chmod +x ./cleanup.sh
+    #     echo ""
+    #     echo "You can delete the server instance by executing:"
+    #     echo "./cleanup.sh"
+    #     echo ""
+    #     echo "Note:"
+    #     echo "You will need to manually delete your private and public keys in ~/.ssh/ folder"
+    #     echo "...or reuse them."
+       
+    #   EOT
+    # }
 
 }
 
+
+resource "null_resource" "cleanup_script" {
+  # This resource is used only to capture information before deletion
+  depends_on = [linode_instance.vpn_instance]
+
+  count = length(var.vpn_instances)
+  triggers = {
+    vpn_host = var.vpn_instances[count.index]["vpn_client_name" ]
+    vpn_hostname = linode_instance.vpn_instance[count.index].ip_address
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<EOT
+      echo 'Storing information about the instance to be deleted'
+      echo "Instance to be deleted host: $VPN_HOST" >> ./deleted_instance_info.txt
+      echo "Instance to be deleted hostname: $VPN_HOSTNAME" >> ./deleted_instance_info.txt
+    EOT
+     environment = {
+      VPN_HOST = "${self.triggers.vpn_host}",
+      VPN_HOSTNAME = "${self.triggers.vpn_hostname}",
+    }
+  }
+}
